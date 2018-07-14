778cc36a8e7e595f387e19a5efe1779e0eea2bac
rennurb3000 <thomas142@gmx.at>
2018-07-14 13:46:53 +0200

    testing the TCP server

---
+++ res/system/server.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var srv  = TCP_Server.new()
+var port = 8080
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("ready")
+
+func _init():
+	srv.listen(port)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var cn = srv.take_connection()
+	if(cn != null):
+		print(str(cn.get_partial_data(64)[1].get_string_from_ascii()))
