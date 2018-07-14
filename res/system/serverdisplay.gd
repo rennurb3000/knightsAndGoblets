865866b0363b73f7695c0ebd7a3d66905eed825f
rennurb3000 <thomas142@gmx.at>
2018-07-14 14:58:09 +0200

    testscene for TCP server

--- res/system/serverdisplay.gd
+++ res/system/serverdisplay.gd
@@ -7,7 +7,7 @@ extends RichTextLabel
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("label ready")
-	var n = get_node("../server")
+	var n = get_node("../../server")
 	n.connect("request",self,"_on_modify_text")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
